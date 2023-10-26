2ff897f72c7df4a3d8ad2e77aeb2113ba2bc8f24
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-10-26 15:59:50 +0200

    changes

---
+++ AtomicModel/Assets/CreateText.cs
@@ -0,0 +1,44 @@
+using ExampleAssets.Business_Logic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateText : MonoBehaviour
+{
+    JsonParser jsonParser;
+    ModelLoader loader;
+    GameObject wrapper;
+
+    public void LoadDescription(Image image)
+    {
+        var Name = image.sprite.name[(image.sprite.name.LastIndexOf('-') + 1)..];
+        Debug.Log(Name);
+        Element element = jsonParser.GetElementByName(Name);
+        loader.DownloadFile(element.Summary);
+        Debug.Log(element.Summary);
+    }
+    void centerGameObject(GameObject gameOBJToCenter, Camera cameraToCenterOBjectTo, float zOffset = 2.1f)
+    {
+        gameOBJToCenter.transform.position = cameraToCenterOBjectTo.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, cameraToCenterOBjectTo.nearClipPlane + zOffset));
+        gameOBJToCenter.transform.SetParent(wrapper.transform);
+        gameOBJToCenter.transform.localScale = new Vector3(3, 3, 3);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        jsonParser = new JsonParser();
+        loader = GetComponent<ModelLoader>();
+        wrapper = new GameObject
+        {
+            name = "Description"
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
