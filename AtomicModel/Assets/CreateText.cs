c7e3ad017eb6f824d444b74c0f4bc6ac84ac4258
Nick0973 <nicolaecer@gmail.com>
2023-10-29 02:23:02 +0200

    update: small card updates

--- AtomicModel/Assets/CreateText.cs
+++ AtomicModel/Assets/CreateText.cs
@@ -10,64 +10,71 @@ using UnityEngine.UI;
 public class CreateText : MonoBehaviour
 {
     CultureInfo cultureInfo;
-    JsonParser jsonParser;
-    ModelLoader loader;
     GameObject wrapper;
     [SerializeField]
     SceneLoader sceneLoader;
-    [SerializeField]
+    Image _image;
     new TextMeshPro name;
-    [SerializeField]
     TextMeshPro category;
-    [SerializeField]
     TextMeshPro appearance;
-    [SerializeField]
     TextMeshPro atomicMass;
-    [SerializeField]
     TextMeshPro electronConfiguration;
-    [SerializeField]
     TextMeshPro meltingPoint;
-    [SerializeField]
     TextMeshPro boilingPoint;
-    [SerializeField]
     TextMeshPro density;
-    [SerializeField]
     TextMeshPro discoveredBy;
-    [SerializeField]
     TextMeshPro description;
 
-
-     public void LoadDescription()
-    {
-        Debug.Log(sceneLoader.element.Summary);
+    public GameObject prefabToLoad;
+    private GameObject instantiatedPrefab;
+     public void LoadDescription(Image image)
+     { 
+         _image.sprite = image.sprite;
         name.SetText(sceneLoader.element.Name);
         category.SetText(sceneLoader.element.Category);
         appearance.SetText(sceneLoader.element.Appearance);
         atomicMass.SetText(sceneLoader.element.AtomicMass.ToString(cultureInfo));
         electronConfiguration.SetText(sceneLoader.element.ElectronConfiguration);
-        meltingPoint.SetText(sceneLoader.element.Melt.ToString());
+        if (sceneLoader.element.Melt is null or 0)
+        {
+            meltingPoint.SetText("unknown");
+        }
+        else {meltingPoint.SetText(sceneLoader.element.Melt.ToString());}
         boilingPoint.SetText(sceneLoader.element.Boil.ToString());
         density.SetText(sceneLoader.element.Density.ToString());
         discoveredBy.SetText(sceneLoader.element.DiscoveredBy);
-        description.SetText(sceneLoader.element.Summary); 
+        description.SetText(sceneLoader.element.Summary);
+        if (instantiatedPrefab != null)
+        {
+            instantiatedPrefab.SetActive(true);
+        }
+        centerGameObject(instantiatedPrefab, Camera.main);
     }
     void centerGameObject(GameObject gameOBJToCenter, Camera cameraToCenterOBjectTo, float zOffset = 2.1f)
     {
-        gameOBJToCenter.transform.position = cameraToCenterOBjectTo.ViewportToWorldPoint(new Vector3(-3f, 2f, cameraToCenterOBjectTo.nearClipPlane + zOffset));
-        gameOBJToCenter.transform.SetParent(wrapper.transform);
-        gameOBJToCenter.transform.localScale = new Vector3(3, 3, 3);
-
+        gameOBJToCenter.transform.position = cameraToCenterOBjectTo.ViewportToWorldPoint(new Vector3(7f, 3f, cameraToCenterOBjectTo.nearClipPlane + zOffset));
     }
-    // Start is called before the first frame update
     void Start()
     {   cultureInfo = new CultureInfo("en-US");
-        jsonParser = new JsonParser();
-        loader = GetComponent<ModelLoader>();
-        wrapper = new GameObject
-        {
-            name = "Description"
-        };
+        instantiatedPrefab = Instantiate(prefabToLoad, new Vector3(0, 0, 0), Quaternion.identity);
+        Canvas canvas = instantiatedPrefab.GetComponentInChildren<Canvas>();
 
+        if (canvas != null)
+        {
+            Camera eventCamera = Camera.main; 
+            canvas.worldCamera = eventCamera;
+            _image = canvas.transform.Find("Background/Image").GetComponent<Image>();
+            name = canvas.transform.Find("Background/Name").GetComponent<TextMeshPro>();
+            category = canvas.transform.Find("Background/Category").GetComponent<TextMeshPro>();
+            appearance = canvas.transform.Find("Background/Appearance").GetComponent<TextMeshPro>();
+            atomicMass = canvas.transform.Find("Background/Atomic Mass").GetComponent<TextMeshPro>();
+            electronConfiguration = canvas.transform.Find("Background/Electron Configuration").GetComponent<TextMeshPro>();
+            meltingPoint = canvas.transform.Find("Background/Melting Point").GetComponent<TextMeshPro>();
+            boilingPoint = canvas.transform.Find("Background/Boiling Point").GetComponent<TextMeshPro>();
+            density = canvas.transform.Find("Background/Density").GetComponent<TextMeshPro>();
+            discoveredBy = canvas.transform.Find("Background/Discovered By").GetComponent<TextMeshPro>();
+            description = canvas.transform.Find("Background/Summary").GetComponent<TextMeshPro>();
+        }
     }
 
 
